e6d82df098ac85902ca867d11a094d1592a65573
Alf <cecerealfonso98@gmail.com>
2023-11-25 10:22:21 +0100

    animazione M non infinita, animation no-transition

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,17 +12,18 @@ window.onload = function () {
   const array = [];
   const ggs = document.querySelectorAll("g[style='display: block']");
 
+  let j = 0;
   const hide = setInterval(function () {
     const rand = Math.round(Math.random() * ggs.length + 1);
-    if (ggs[rand].getAttribute("opacity") == 1) {
-      ggs[rand].setAttribute("opacity", 0);
+    if (ggs[rand].hasAttribute("opacity") && ggs[rand].getAttribute("opacity") == 1) {
+      console.log(j);
+      ggs[rand].style.animation = "fade 5s 1000ms both";
+      ggs[rand].style["animation-timing-function"] = "cubic-bezier(0.25,1.89,0.25,1.89)";
     }
-  }, 100);
+    j++;
+  }, 10);
 
-  const show = setInterval(function () {
-    const rand = Math.round(Math.random() * ggs.length + 1);
-    if (ggs[rand].getAttribute("opacity") == 0) {
-      ggs[rand].setAttribute("opacity", 1);
-    }
-  }, 50);
+  setTimeout(() => {
+    clearInterval(hide);
+  }, 2000);
 };
